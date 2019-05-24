df1a72ec5dd184a8c660f9d42222f1d7f7a79eec
Perminus-Gaita <35493600+Perminus-Gaita@users.noreply.github.com>
2019-05-24 14:47:42 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require('./config/mongoose');
 // module.parent check is required to support mocha watch
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
-  app.listen(config.port/*'80','192.168.1.5'*/, () => {
+  app.listen(config.port, () => {
     console.info(`server started on port ${config.port} (${config.env})`);
   });
 }
